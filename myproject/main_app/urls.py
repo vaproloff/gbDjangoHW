a8da67073530e6ec9b0f17ce2fdfeddd883fa3f2
Konstantin Pivnov <vaproloff@ya.ru>
2023-09-27 23:16:05 +0300

    add base template and extending main pages

--- myproject/main_app/urls.py
+++ myproject/main_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('about/', views.AboutView.as_view(), name='about'),
 ]
--- myproject/main_app/views.py
+++ myproject/main_app/views.py
@@ -1,64 +1,19 @@
-import logging
-from django.http import HttpResponse
-
-logger = logging.getLogger(__name__)
+from django.views.generic import TemplateView
 
 
-def index(request):
-    html = '''
-        <!DOCTYPE html>
-        <html lang="ru">
-        <head>
-            <meta charset="UTF-8">
-            <title>Главная</title>
-        </head>
-        <body>
-        <header>
-            <ul>
-                <li><a href="/">Главная</a></li>
-                <li><a href="/about">О себе</a></li>
-            </ul>
-        </header>
-        <main>
-            <h1>Мой первый сайт на Django!</h1>
-        </main>
-        <footer>
-            <p><em>© Copyright 2023</em></p>
-        </footer>
-        </body>
-        </html>'''
+class IndexView(TemplateView):
+    template_name = 'main_app/index.html'
 
-    logger.info(f'Открыта Главная страница')
-    return HttpResponse(html)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная'
+        return context
 
 
-def about(request):
-    html = '''
-        <!DOCTYPE html>
-        <html lang="ru">
-        <head>
-            <meta charset="UTF-8">
-            <title>О себе</title>
-        </head>
-        <body>
-        <header>
-            <ul>
-                <li><a href="/">Главная</a></li>
-                <li><a href="/about">О себе</a></li>
-            </ul>
-        </header>
-        <main>
-            <h1>Немного обо мне</h1>
-            <p>
-                Неужели писать верстку HTML - задача бекенд-разработчика?<br>
-                Вот и я так не считаю. Конец.
-            </p>
-        </main>
-        <footer>
-            <p><em>© Copyright 2023</em></p>
-        </footer>
-        </body>
-        </html>'''
+class AboutView(TemplateView):
+    template_name = 'main_app/about.html'
 
-    logger.info(f'Открыта страница О себе')
-    return HttpResponse(html)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'О себе'
+        return context
